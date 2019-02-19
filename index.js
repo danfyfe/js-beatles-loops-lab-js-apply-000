543b4ad96aff8df70a1b5edc0feba52a317b6475
Dan Fyfe <fyfedaniel@gmail.com>
2019-02-19 22:04:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = []
-  i = 0;
-  for (var i = 0; i < 4; i++){
-    array.push(musicians[i] + " plays " + instruments[i])
-  }
-  return array
-}
-
-
-
-function johnLennonFacts(array){
-  var factsArray = []
-  while(array.length > 0){
-    factsArray.push(array.shift() + "!!!")
-  }
-  return factsArray;
-}
-
-
-function iLoveTheBeatles(num){
   var array = [];
-  
-  var i = 0;
- 
-function incrementVariable() {
-  i = i + 1;
-  return i;
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
 }
 
-do {
-  array.push("I love the Beatles!")
-} while (incrementVariable() < 15 - num)
-return array
-}
---
+++ indexDFPASSED.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < 4; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  var factsArray = []
+  while (array.length > 0){
+    factsArray.push(array.shift() + "!!!")
+  }
+  return factsArray
+}
+
+function iLoveTheBeatles(num){
+  var array = []
+  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  
+  do {
+    array.push("I love the Beatles!")
+  } while (incrementVariable() < 15 - num)
+  return array
+}
